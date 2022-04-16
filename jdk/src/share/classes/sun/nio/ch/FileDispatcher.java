e1636e610e21f927f6f2f88e57bdc9b3ec5e2560
541711153@qq.com <541711153lcc>
2022-04-17 00:11:12 +0800

    feature: 通道-分派器

--- jdk/src/share/classes/sun/nio/ch/FileDispatcher.java
+++ jdk/src/share/classes/sun/nio/ch/FileDispatcher.java
@@ -29,6 +29,8 @@ import java.io.FileDescriptor;
 import java.io.IOException;
 import java.nio.channels.SelectableChannel;
 
+
+// File中IO操作分派器的抽象实现
 abstract class FileDispatcher extends NativeDispatcher {
 
     public static final int NO_LOCK = -1;       // Failed to lock
@@ -36,15 +38,24 @@ abstract class FileDispatcher extends NativeDispatcher {
     public static final int RET_EX_LOCK = 1;    // Obtained exclusive lock
     public static final int INTERRUPTED = 2;    // Request interrupted
 
+    /*
+     * 设置实时同步
+     * metaData为true 时，将文件内容或元数据的每个更新都实时同步到底层设备，类似于RandomAccessFile的rws模式
+     * metaData为false时，将文件内容的每个更新都实时同步到底层设备，类似于RandomAccessFile的rwd模式
+     */
     abstract int force(FileDescriptor fd, boolean metaData) throws IOException;
 
+    // 用新尺寸size截短/扩展通道(文件)
     abstract int truncate(FileDescriptor fd, long size) throws IOException;
 
+    // 返回fd处文件的大小(以字节为单位)
     abstract long size(FileDescriptor fd) throws IOException;
 
+    // 对fd处的文件申请文件锁；加锁范围是pos处起的size个字节；shared指示是否申请共享锁；blocking指示是否阻塞式加锁
     abstract int lock(FileDescriptor fd, boolean blocking, long pos, long size,
                        boolean shared) throws IOException;
 
+    // 对fd处的文件释放文件锁；解锁范围是pos处起的size个字节
     abstract void release(FileDescriptor fd, long pos, long size)
         throws IOException;
 
@@ -53,10 +64,13 @@ abstract class FileDispatcher extends NativeDispatcher {
      * memory-mapping operations, otherwise returns an invalid
      * FileDescriptor (meaning a newly allocated FileDescriptor)
      */
+    // 复制一份文件描述符
     abstract FileDescriptor duplicateForMapping(FileDescriptor fd)
         throws IOException;
 
+    // 系统是否支持直接从文件通道向socket通道传输数据(windows上默认不支持，类unix上默认支持)
     abstract boolean canTransferToDirectly(SelectableChannel sc);
 
+    // 系统直接从文件通道向socket通道传输数据时是否需要锁定游标敏感的操作(windows上默认需要，类unix上默认不需要)
     abstract boolean transferToDirectlyNeedsPositionLock();
 }
